137b067154718668b8ca0778935a0a56058277b8
pikapika-zrf <1422424904@qq.com>
2023-12-10 15:18:12 +0800

    add ukf

---
+++ 参数辨识/uavMeasurementFcn1.m
@@ -0,0 +1,8 @@
+function [phi_Predicted]  = uavMeasurementFcn1(state_Predicted,w)
+
+phi_Predicted = state_Predicted(1);
+% theta_Predicted = state_Predicted(4);
+% psi_Predicted = state_Predicted(7);
+% Ix_Predicted = state_Predicted(10);
+% Iy_Predicted = state_Predicted(12);
+end
